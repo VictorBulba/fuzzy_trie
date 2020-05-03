24959cbae2f9c90eb8f3787f03aec797f55e71af
VictorBulba <bulba@foliro.com>
2020-05-03 12:49:53 +0300

    Eliminate tabs

--- src/inserter.rs
+++ src/inserter.rs
@@ -3,21 +3,21 @@ use crate::Node;
 
 /// Trick to make possible using a value field as key
 pub struct Inserter<'a, T> {
-	values: &'a mut Vec<T>,
-	to: &'a mut Vec<Node>,
+    values: &'a mut Vec<T>,
+    to: &'a mut Vec<Node>,
 }
 
 
 impl<'a, T> Inserter<'a, T> {
-	/// Consume self and insert a value
-	#[inline]
-	pub fn insert(self, value: T) {
-		self.values.push(value);
-		let index = self.values.len() - 1;
-		self.to.push(Node::new_value_index(index));
-	}
+    /// Consume self and insert a value
+    #[inline]
+    pub fn insert(self, value: T) {
+        self.values.push(value);
+        let index = self.values.len() - 1;
+        self.to.push(Node::new_value_index(index));
+    }
 
-	pub(crate) fn new(values: &'a mut Vec<T>, to: &'a mut Vec<Node>) -> Self {
-		Self{values, to}
-	}
+    pub(crate) fn new(values: &'a mut Vec<T>, to: &'a mut Vec<Node>) -> Self {
+        Self{values, to}
+    }
 }
